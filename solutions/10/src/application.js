e811bae1a11fdd6a3756f1b25b87d099e89b9a33
Antimony <146525840+nofriendsatall@users.noreply.github.com>
2025-05-27 18:27:10 +0300

    Add files via upload

--- solutions/10/src/application.js
+++ solutions/10/src/application.js
@@ -1,5 +1,25 @@
 import escapeHtml from 'escape-html';
 
 // BEGIN
+export default function () {
+  const form = document.querySelector('form');
+  form.addEventListener('submit', function (event) {
 
+    const { email, name, comment } = event.target.elements;
+    const escapedEmail = escapeHtml(email.value);
+    const escapedName = escapeHtml(name.value);
+    const escapedComment = escapeHtml(comment.value);
+
+    const content = `
+      <div>
+        <p>Feedback has been sent</p>
+        <div>Email: ${escapedEmail}</div>
+        <div>Name: ${escapedName}</div>
+        <div>Comment: ${escapedComment}</div>
+      </div>
+    `;
+
+    form.outerHTML = content;
+  });
+}
 // END
